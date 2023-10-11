46747ed5c6aac610cbdd172a369a5cba58a79de6
eimanhakimy <hakimypro55@gmail.com>
2023-10-11 11:03:41 +0800

    Tambah gambar

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -1,11 +1,13 @@
 
     <div class="container">
 
+   
+
         <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
-                <div class="col-lg-5 d-none d-lg-block bg-login-image" style="background-image: url('assets/img/polis.jpg');"></div>
+                <div class="col-lg-5 d-none d-lg-block bg-login-image" style="background-image: url('../assets/img/bahanbukti.png');"></div>
 
                     <div class="col-lg-7">
                         <div class="p-5">
@@ -50,7 +52,6 @@
                 </div>
             </div>
         </div>
-
-    </div>
+</div>
 
    
